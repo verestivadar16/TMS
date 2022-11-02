2e21c4e33d146119aaca39b3c15aec0f41f144c3
Tomi08 <79204444+Tomi08@users.noreply.github.com>
2022-11-02 17:48:42 +0200

    forgot password page

--- app/src/main/java/com/example/tms/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/tms/view/activity/MainActivity.kt
@@ -12,28 +12,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.forgot_password)
-//        // calling the action bar
-//        var actionBar = getSupportActionBar()
-//
-//        if (actionBar != null) {
-//
-//            // Customize the back button
-//            actionBar.setHomeAsUpIndicator(R.drawable.mybutton);
-//
-//            // showing the back button in action bar
-//            actionBar.setDisplayHomeAsUpEnabled(true);
-//        }
-//    }
-//
-//    // this event will enable the back
-//    // function to the button on press
-//    override fun onContextItemSelected(item: MenuItem): Boolean {
-//        when (item.itemId) {
-//            android.R.id.home -> {
-//                finish()
-//                return true
-//            }
-//        }
-//        return super.onContextItemSelected(item)
     }
     }
