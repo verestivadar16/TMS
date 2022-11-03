d86eed94c07b988442b2c4ff7703421a96532936
[tivike16] <verestivadar19@gmail.com>
2022-11-03 21:29:17 +0200

    Finished the branch

--- app/src/main/java/com/example/tms/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/tms/view/activity/MainActivity.kt
@@ -5,12 +5,22 @@ package com.example.tms.view.activity
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.tms.R
+import com.example.tms.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.forgot_password)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
-    }
+}
---
+++ app/src/main/java/com/example/tms/view/fragment/StartFragment.kt
@@ -0,0 +1,29 @@
+package com.example.tms.view.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.tms.R
+import com.example.tms.databinding.StartPageBinding
+
+class StartFragment : Fragment() {
+    private lateinit var binding : StartPageBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+
+        binding = StartPageBinding.inflate(layoutInflater)
+
+        binding.loginButton.setOnClickListener(View.OnClickListener{
+            findNavController().navigate(R.id.action_startFragment_to_loginFragment)
+        })
+        binding.registerButton.setOnClickListener{
+            findNavController().navigate((R.id.action_startFragment_to_registerFragment))
+        }
+        return binding.root
+    }
+
+}
