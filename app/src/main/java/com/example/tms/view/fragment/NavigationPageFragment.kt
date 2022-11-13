678a109ed3eed831930dc64e631adc809a08c310
[rolad0822] <79204415+roland0822@users.noreply.github.com>
2022-11-13 14:24:16 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tms/view/fragment/NavigationPageFragment.kt
@@ -0,0 +1,21 @@
+package com.example.tms.view.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.tms.databinding.NavigationPageBinding
+
+class NavigationPageFragment :Fragment(){
+
+    private lateinit var binding: NavigationPageBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding=NavigationPageBinding.inflate(layoutInflater)
+        return binding.root
+    }
+}
