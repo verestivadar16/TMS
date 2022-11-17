ff75b6bcbce0b797a833543a957402f415d0c1af
Tomi08 <79204444+Tomi08@users.noreply.github.com>
2022-11-17 10:36:38 +0200

    inbox scrollable conversation list with testing

--- app/src/main/java/com/example/tms/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/tms/view/activity/MainActivity.kt
@@ -3,10 +3,35 @@ package com.example.tms.view.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tms.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tms.view.fragment.ConversationViewModel
+import com.example.tms.view.fragment.CustomAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.inbox_page)
+        // getting the recyclerview by its id
+        val recyclerview = findViewById<RecyclerView>(R.id.conversation_list)
+
+        // this creates a vertical layout Manager
+        recyclerview.layoutManager = LinearLayoutManager(this)
+
+        // ArrayList of class ItemsViewModel
+        val data = ArrayList<ConversationViewModel>()
+
+        // This loop will create 20 Views containing
+        // the image with the count of view
+        for (i in 1..20) {
+            data.add(ConversationViewModel(R.drawable.avatar, "Item " + i,"igen","12:00"))
+        }
+
+        // This will pass the ArrayList to our Adapter
+        val adapter = CustomAdapter(data)
+
+        // Setting the Adapter with the recyclerview
+        recyclerview.adapter = adapter
     }
+
 }
---
+++ app/src/main/java/com/example/tms/view/fragment/ConversationViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.tms.view.fragment
+
+data class ConversationViewModel(val image: Int, val name: String, val message: String, val time: String) {
+
+
+}
