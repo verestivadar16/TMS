2ce2b2cba5db3c74f4ed18c2f07b02fc5076c96d
[rolad0822] <79204415+roland0822@users.noreply.github.com>
2022-12-17 18:43:30 +0200

    update commit

--- app/src/main/java/com/example/tms/view/fragment/NavigationPageFragment.kt
+++ app/src/main/java/com/example/tms/view/fragment/NavigationPageFragment.kt
@@ -1,9 +1,17 @@
 package com.example.tms.view.fragment
 
+import android.Manifest
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.requestPermissions
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.tms.R
@@ -14,10 +22,10 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-
 //AIzaSyCXPxzAnc8icMuBAYqWnbtw5S2eaT5opMg
 
 class NavigationPageFragment : Fragment(), OnMapReadyCallback {
+//    private lateinit var REQUEST_LOCATION
     private lateinit var binding: NavigationPageBinding
     private lateinit var mMap: GoogleMap
     override fun onCreateView(
@@ -25,6 +33,8 @@ class NavigationPageFragment : Fragment(), OnMapReadyCallback {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Manifest.permission.ACCESS_FINE_LOCATION
+        PackageManager.PERMISSION_GRANTED
 
         binding = NavigationPageBinding.inflate(layoutInflater)
 //        DirectionAPI.openGoogleMapsNavigationFromAToB(this, -34.0, 151.0, -30.0, 150.0)
@@ -52,7 +62,39 @@ class NavigationPageFragment : Fragment(), OnMapReadyCallback {
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == AppConstant.LOCATION_PERMISSION) {
+
+            if (permissions.isEmpty() && grantResults.isEmpty()) {
+                return
+            }
+
+            if (permissions[0] ==
+                Manifest.permission.ACCESS_FINE_LOCATION &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED
+            ) {
+                // do something which you want to do
+                // when permission granted
+            }
+            else if (permissions[0] ==
+                Manifest.permission.ACCESS_FINE_LOCATION &&
+                grantResults[0] == PackageManager.PERMISSION_DENIED
+            ) {
+                // if permission denied then check whether never ask
+                // again is selected or not by making use of
+                !ActivityCompat.shouldShowRequestPermissionRationale(
+                    requireActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        }
+    }
+
 }
+
 
 
 
