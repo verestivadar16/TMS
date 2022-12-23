3bd1684ba447cb85b78b651eddaa941f8910fed2
[tivike16] <verestivadar19@gmail.com>
2022-12-23 21:57:48 +0200

    Select image done

--- app/src/main/java/com/example/tms/view/fragment/InstaUploadFragment.kt
+++ app/src/main/java/com/example/tms/view/fragment/InstaUploadFragment.kt
@@ -1,9 +1,13 @@
 package com.example.tms.view.fragment
 
+import android.app.Activity.RESULT_OK
 import android.app.AlertDialog
+import android.app.ProgressDialog
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,9 +15,14 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.tms.R
 import com.example.tms.databinding.InstaUploadImageBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.text.SimpleDateFormat
+import java.util.*
 
 class InstaUploadFragment : Fragment() {
     private lateinit var binding: InstaUploadImageBinding
+    private lateinit var imageUri: Uri
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,9 +34,68 @@ class InstaUploadFragment : Fragment() {
         binding.imageButtonBack.setOnClickListener(View.OnClickListener {
             findNavController().navigate(R.id.action_instauploadpage_to_instapostpage)
         })
-        binding.buttonChooseImage.setOnClickListener({})
+        binding.buttonChooseImage.setOnClickListener(View.OnClickListener{
+
+            selectImage()
+
+        })
+
+        binding.buttonUpload.setOnClickListener(){
+
+            uploadImage()
+
+        }
         return binding.root
 
+
+    }
+
+
+
+    private fun uploadImage() {
+
+        val progressDialog = ProgressDialog(requireContext())
+        progressDialog.setMessage("Uploading file ...")
+        progressDialog.setCancelable(false)
+        progressDialog.show()
+
+        val db = Firebase.firestore
+
+        val user = hashMapOf(
+            "first" to "Alan",
+            "middle" to "Mathison",
+            "last" to "Turing",
+            "born" to 1912
+        )
+
+// Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+
+    }
+    private fun selectImage() {
+
+        val intent = Intent()
+        intent.type= "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+
+        startActivityForResult(intent,100)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 100 && resultCode == RESULT_OK){
+            imageUri=data?.data!!
+            binding.uploadedImage.setImageURI(imageUri)
+        }
 
     }
 }
