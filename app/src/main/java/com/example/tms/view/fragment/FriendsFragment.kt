e2db65c1dc5cdc1626a25123cef8cd396d98ffa6
[tivike16] <verestivadar19@gmail.com>
2023-01-02 21:08:51 +0200

    Implemented friend list direct message

--- app/src/main/java/com/example/tms/view/fragment/FriendsFragment.kt
+++ app/src/main/java/com/example/tms/view/fragment/FriendsFragment.kt
@@ -8,8 +8,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResult
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.tms.R
 import com.example.tms.adapter.FriendListAdapter
 import com.example.tms.data.FriendListData
@@ -18,7 +22,6 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
-import com.google.protobuf.LazyStringArrayList
 import java.io.File
 
 class FriendsFragment : Fragment() {
@@ -40,75 +43,98 @@ class FriendsFragment : Fragment() {
             findNavController().navigate(R.id.action_friends_page_to_add_friend_page)
         })
 
-        //test this needs to be implemented
-
-        requestFriends()
 
         return binding.root
     }
 
-    private fun requestFriends(){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerview = getView()?.findViewById<RecyclerView>(R.id.friends_list)
+        if (recyclerview != null) {
+            recyclerview.layoutManager = LinearLayoutManager(getContext())
+        }
+        val data = ArrayList<FriendListData>()
         val db = Firebase.firestore
-
-        friendArrayList = ArrayList()
-       // db.collection("users").whereArrayContains("regions", listOf(mAuth.currentUser?.uid))
-
 
         val docRef = db.collection("users").document(mAuth.currentUser?.uid.toString())
 
         docRef.get()
-                .addOnSuccessListener { document ->
-                    if (document != null) {
-                        try {
-                            val friends = document["friends"] as ArrayList<*>
-                            for(it in friends){
-                                val docRef2 = db.collection("users").document(it.toString())
-                                docRef2.get()
-                                        .addOnSuccessListener { document2 ->
-                                            if (document2 != null) {
-                                                try {
-                                                    val userName = document2.getString("userName")!!
-                                                    val imageName = document2.getString("profileImage")!!
-                                                    val storageRef = FirebaseStorage.getInstance().reference.child("images/$imageName")
-                                                    val localFile = File.createTempFile("tempImage", "jpg")
-                                                    storageRef.getFile(localFile).addOnSuccessListener {
-                                                        val bitmap = BitmapFactory.decodeFile(localFile.absolutePath)
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    try {
+                        val friends = document["friends"] as ArrayList<*>
+                        for(it in friends){
+                            val uid = it.toString()
 
-                                                        val contact = FriendListData(userName, bitmap)
-                                                        friendArrayList.add(contact)
-                                                        friendArrayList.sortBy { it.username }
-                                                        binding.listview.adapter = activity?.let { FriendListAdapter(it, friendArrayList)}
+                            val docRef2 = db.collection("users").document(it.toString())
 
-                                                    }
+                            docRef2.get()
+                                .addOnSuccessListener { document2 ->
+                                    if (document2 != null) {
+                                        try {
+                                            val userName = document2.getString("userName")!!
+                                            val imageName = document2.getString("profileImage")!!
+                                            val storageRef = FirebaseStorage.getInstance().reference.child("images/$imageName")
+                                            val localFile = File.createTempFile("tempImage", "jpg")
 
-                                                }catch (e:Exception){
-                                                    Toast.makeText(requireContext(),"Friends cant be loaded!",Toast.LENGTH_SHORT).show()
+                                            storageRef.getFile(localFile).addOnSuccessListener {
+                                                val bitmap = BitmapFactory.decodeFile(localFile.absolutePath)
+
+                                                val contact = FriendListData(uid,userName, bitmap)
+                                                data.add(contact)
+                                                val adapter = FriendListAdapter(data)
+                                                if (recyclerview != null) {
+                                                    recyclerview.adapter = adapter
+                                                }
+                                                if (recyclerview != null) {
+                                                    recyclerview.adapter = adapter
                                                 }
 
-                                                Log.d(ContentValues.TAG, "DocumentSnapshot data: ${document.data}")
-                                            } else {
-                                                Log.d(ContentValues.TAG, "No such document")
+
+                                                adapter.onItemClick = { it2 ->
+
+                                                    val result = it2.uid
+
+                                                    setFragmentResult("requestKey", bundleOf(("uid" to result)))
+                                                    setFragmentResult("requestKey2", bundleOf(("userID" to result)))
+
+                                                    findNavController().navigate(R.id.action_friends_page_to_chatpage)
+                                                }
+
+
                                             }
-                                        }
-                                        .addOnFailureListener { exception ->
-                                            Log.d(ContentValues.TAG, "get failed with ", exception)
+
+                                        }catch (e:Exception){
+                                            Toast.makeText(requireContext(),"Friends cant be loaded!",Toast.LENGTH_SHORT).show()
                                         }
 
-                            }
+                                        Log.d(ContentValues.TAG, "DocumentSnapshot data: ${document.data}")
+                                    } else {
+                                        Log.d(ContentValues.TAG, "No such document")
+                                    }
+                                }
+                                .addOnFailureListener { exception ->
+                                    Log.d(ContentValues.TAG, "get failed with ", exception)
+                                }
 
-                        }catch (e:Exception){
-                            Toast.makeText(requireContext(),"No friends yet!",Toast.LENGTH_SHORT).show()
                         }
 
-
-                        Log.d(ContentValues.TAG, "DocumentSnapshot data: ${document.data}")
-                    } else {
-                        Log.d(ContentValues.TAG, "No such document")
+                    }catch (e:Exception){
+                        Toast.makeText(requireContext(),"No friends yet!",Toast.LENGTH_SHORT).show()
                     }
+
+
+                    Log.d(ContentValues.TAG, "DocumentSnapshot data: ${document.data}")
+                } else {
+                    Log.d(ContentValues.TAG, "No such document")
                 }
-                .addOnFailureListener { exception ->
-                    Log.d(ContentValues.TAG, "get failed with ", exception)
-                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d(ContentValues.TAG, "get failed with ", exception)
+            }
+
+
 
     }
 
